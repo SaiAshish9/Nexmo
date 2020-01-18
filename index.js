d14b0262f390dd2069a50a588c01bea0c6854cb1
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2020-01-18 16:03:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express=require('express')
 const app=express()
 
 
-const accountSid = 'ACb4c2180b0b5d3635174c97bf80f48743';
-const authToken = 'a196629698fd0e4ca151ab2bb6b36324';
+const accountSid = '';
+const authToken = '';
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
      from: '+12054311041',
-     to: '+918920125544'
+     to: ''
    })
   .then(message => console.log(message));
   // .sid
